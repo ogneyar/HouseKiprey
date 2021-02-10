1c38a8cb31e1f448f11c83c30830790759bd6aa1
ogneyar <ogneyar-ne@ya.ru>
2021-02-10 22:15:01 +0300

    eee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { TestsComponent } from './tests/tests.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tests/tests.component.ts
+++ src/app/tests/tests.component.ts
@@ -1,23 +1,51 @@
-import { AnimationBuilder } from '@angular/animations';
+import { animate, AnimationBuilder, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import MyCmp from './testAnimations'
+// import MyCmp from './testAnimations'
 
 
 @Component({
   selector: 'app-tests',
   templateUrl: './tests.component.html',
-  styleUrls: ['./tests.component.scss']
+  styleUrls: ['./tests.component.scss'],
+  animations: [
+    trigger('expandedPanel', [
+      state('initial', style({ height: 0 })),
+      state('expanded', style({ height: '*' })),
+      transition('initial <=> expanded', animate('0.3s')),
+    ]),
+  ],
 })
-export class TestsComponent implements OnInit {
+export class TestsComponent {
 
-  constructor() { 
+  constructor() {
+    // let func = () => {
+    //   this.isExpanded = !this.isExpanded
+    //   this.state = this.isExpanded ? 'expanded' : 'initial'
+    // }
+
+    // const t = "true"
+    // for(let in = 0; in < 100; in++) {
+      setTimeout(()=>console.log("logg"), 2000);
+    // }
     
+  
   }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
   // new MyCmp(AnimationBuilder).makeAnimation("h1")
 
+  isExpanded: boolean = false
+  state: string = 'initial'
 
+  expand() {
+    // while(true) {
+      this.isExpanded = !this.isExpanded
+      this.state = this.isExpanded ? 'expanded' : 'initial'
+    // }
+  }
+
+  
+  
 }
