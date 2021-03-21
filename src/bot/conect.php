d10419cfb58451665744ffde30d149d6ac4bbc15
ogneyar <ogneyar-ne@ya.ru>
2021-03-21 18:46:23 +0300

    ууу

--- src/bot/conect.php
+++ src/bot/conect.php
@@ -1,4 +1,5 @@
 <?php
-    $bot_token = getenv('BOT_TOKEN');
-    $bot_webhook = getenv('BOT_WEBHOOK');
+    include_once '.env';
+    // $bot_token = getenv('BOT_TOKEN');
+    // $bot_webhook = getenv('BOT_WEBHOOK');
 ?>
