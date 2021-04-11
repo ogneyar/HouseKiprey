5bcbb45ecbcb8b5cbf44919e8cffcdc786e8cdc7
ogneyar <ogneyar-ne@ya.ru>
2021-04-11 17:29:23 +0300

    add method WebSocket

--- src/app/house-kiprey/consultant/consultant.component.ts
+++ src/app/house-kiprey/consultant/consultant.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultantComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.WebSocket();
+  }
 
   ngOnInit(): void {
+    
   }
   
   open() {
@@ -26,6 +29,38 @@ export class ConsultantComponent implements OnInit {
     consultantForm.style.visibility = "hidden";
     consultantForm.style.display = "none";
     consultantForm.style.opacity = "0";
+  }
+
+  WebSocket() {
+    const socket = new WebSocket('ws://consultant.herokuapp.com/')
+    // canvasState.setSocket(socket)
+    // canvasState.setSessionId(params.id)
+
+    socket.onopen = () => {
+        console.log("Подключение установлено")
+        socket.send(JSON.stringify({
+            method: 'connection',
+            id: "params.id",
+            username: "canvasState.username"
+        }))
+    }
+    socket.onmessage = (event) => {
+        let msg = JSON.parse(event.data)
+        // eslint-disable-next-line
+        switch (msg.method) {
+            case 'connection':
+                console.log(`Пользователь ${msg.username} присоединился`);
+                break
+            case 'draw':
+              this.MessageHandler(msg)
+                break
+
+        }
+    }
+  }
+
+  MessageHandler(msg) {
+    console.log(msg);
   }
 
 }
